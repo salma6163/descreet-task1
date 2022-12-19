cc047b0c3fd34b393a201ff8550ef22f4639c741
salma6163 <121053878+salma6163@users.noreply.github.com>
2022-12-19 20:26:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// problem4
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace problem
+{
+    public static class program
+    {
+        static void Main()
+        {
+            Console.WriteLine("please enter the first number");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter the second number");
+            int n2 = int.Parse(Console.ReadLine());
+
+            for (n1 = 5; n1 <= n2; n1 += 2)
+            {
+                do
+                {
+
+                    Console.WriteLine("prime number is "+n1);
+                    n1 += 2;
+                } while (n1 <= n2 && (n1 % 3 == 1));
+            }
+       
+            
+            
+               
+            
+        
+            
+        }
+       
+    }
+}
